90dd4e131adac74c75e029de4abbb25a2dd6f696
janina <ianinadavydova@gmail.com>
2025-11-03 20:19:03 +0100

    fix linter

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,10 @@
+"""Module for working with strings."""
+
 def output_word_with_dash():
+    """
+    This function is about replacing spaces
+    :return:
+    """
     words = input("Enter 3 words separated by space: ")
     words = words.replace(" ", "-")
     print(words)
